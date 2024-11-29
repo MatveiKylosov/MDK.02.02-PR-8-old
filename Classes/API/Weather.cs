465e9bb549b1e7a0f2a2de3ccd90d2eed16e6f7e
matvei <matveikylosov@gmail.com>
2024-11-29 14:50:36 +0500

    Реализованы классы для взаимодействия с API погоды. Данные классы сначала тестировались в консоли.

---
+++ Classes/API/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kylosov.Classes.API
+{
+    /// <summary>
+    /// Класс, представляющий описание погоды (например, "ясно", "дождь" и т.д.).
+    /// </summary>
+    public class Weather
+    {
+        public string Description { get; set; }  // Описание погоды
+    }
+}
